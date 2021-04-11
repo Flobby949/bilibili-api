c7adc05eda570a6ce4a7eefdb5915f81ae807c48
jinhuiqian <1286122850@qq.com>
2021-04-11 23:28:21 +0800

    数据库实体类

--- src/main/java/top/soft1921/bili/api/mapper/RankMapper.java
+++ src/main/java/top/soft1921/bili/api/mapper/RankMapper.java
@@ -4,7 +4,9 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import top.soft1921.bili.api.model.entity.Rank;
 
 /**
- * @author Flobby
+ * @author 231
+ * @description ${description}
+ * @date 2021-04-11 23:25
  */
 public interface RankMapper extends BaseMapper<Rank> {
 
--- src/main/java/top/soft1921/bili/api/model/entity/Rank.java
+++ src/main/java/top/soft1921/bili/api/model/entity/Rank.java
@@ -7,7 +7,9 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
- * @author Flobby
+ * @author 231
+ * @description ${description}
+ * @date 2021-04-11 23:25
  */
 @Data
 @Builder
@@ -15,72 +17,72 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Rank {
     /**
-    * 视频主键
-    */
+     * 视频主键
+     */
     private Integer id;
 
     /**
-    * up主id
-    */
+     * up主id
+     */
     private Integer userId;
 
     /**
-    * 播放量
-    */
+     * 播放量
+     */
     private Integer view;
 
     /**
-    * 弹幕数量
-    */
+     * 弹幕数量
+     */
     private Integer danmaku;
 
     /**
-    * 收藏数量
-    */
+     * 收藏数量
+     */
     private Integer favorites;
 
     /**
-    * 投币数量
-    */
+     * 投币数量
+     */
     private Integer coins;
 
     /**
-    * 转发数量
-    */
+     * 转发数量
+     */
     private Integer share;
 
     /**
-    * 点赞数量
-    */
+     * 点赞数量
+     */
     private Integer like;
 
     /**
-    * 视频总时长
-    */
+     * 视频总时长
+     */
     private Integer duration;
 
     /**
-    * 发布时间
-    */
+     * 发布时间
+     */
     private Date pubdate;
 
     /**
-    * 视频标题
-    */
+     * 视频标题
+     */
     private String title;
 
     /**
-    * 视频分区
-    */
+     * 视频分区
+     */
     private Byte type;
 
     /**
-    * 视频封面
-    */
+     * 视频封面
+     */
     private String pic;
 
     /**
-    * 综合评分
-    */
+     * 综合评分
+     */
     private Integer score;
 }
